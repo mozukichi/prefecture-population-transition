8c8ffd30a385121137d7789bc4b4fa652403b787
mozukichi <mozukichi.0807@gmail.com>
2020-12-04 12:29:59 +0900

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+};
