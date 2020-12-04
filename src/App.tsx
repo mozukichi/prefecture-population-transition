75f828de7d7b483c9cf5ffa70c2b8d86fc6b342f
mozukichi <mozukichi.0807@gmail.com>
2020-12-04 13:01:14 +0900

    Add header of top page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>都道府県別総人口推移グラフ</h1>
+      </header>
     </div>
   );
 }
