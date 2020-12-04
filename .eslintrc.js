d4e11fb7ae08cd5b03cb6dcb72b4e28d6daeb6b0
mozukichi <mozukichi.0807@gmail.com>
2020-12-04 12:39:46 +0900

    Setup Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+        'prettier/@typescript-eslint',
     ],
 };
