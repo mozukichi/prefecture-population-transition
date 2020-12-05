2e0fa00164080d41c780542db486b1ae771b7744
mozukichi <mozukichi.0807@gmail.com>
2020-12-05 12:31:27 +0900

    Add Prefecture component

---
+++ src/Prefecture.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export type Prefecture = {
+  prefCode: number;
+  prefName: string;
+};
+
+type Props = {
+  prefectures: Prefecture[] | null;
+};
+
+/**
+ * 都道府県チェックボックスリスト
+ */
+export const PrefectureSelector: React.FC<Props> = (props) => {
+  if (props.prefectures === null) {
+    return <p>Prefectures is nothing.</p>;
+  }
+  const prefectureCheckboxes = props.prefectures.map((prefecture) => (
+    <React.Fragment key={prefecture.prefCode}>
+      <label>{prefecture.prefName}</label>
+      <input type="checkbox"></input>
+    </React.Fragment>
+  ));
+  return <div>{prefectureCheckboxes}</div>;
+};
