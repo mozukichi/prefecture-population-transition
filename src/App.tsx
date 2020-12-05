f2730fe1f8c425b62e078b8e5b5fdb6a69fdd0e4
mozukichi <mozukichi.0807@gmail.com>
2020-12-05 12:32:08 +0900

    Add placement Prefecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { Prefecture, PrefectureSelector } from "./Prefecture";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [prefectures, setPrefectures] = useState<Prefecture[] | null>(null);
+
+  useEffect(() => {
+    setPrefectures([
+      { prefCode: 1, prefName: "北海道" },
+      { prefCode: 2, prefName: "青森県" },
+    ]);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>都道府県別総人口推移グラフ</h1>
       </header>
+      <PrefectureSelector prefectures={prefectures}></PrefectureSelector>
     </div>
   );
-}
+};
 
 export default App;
