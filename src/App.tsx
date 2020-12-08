cb9f249930b32edee5ba8b3568f5de62bbc4008a
mozukichi <mozukichi.0807@gmail.com>
2020-12-08 13:04:53 +0900

    Add fetch RESAS-API population data

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import {
   fetchResasPrefectures,
   fetchResasPopulation,
   ResasPopulationData,
+  ResasPopulationResponse,
 } from "./resas";
 import { Prefecture, PrefectureSelector } from "./Prefecture";
 import "./App.css";
 
+/**
+ * アプリ本体のコンポーネント
+ */
 const App: React.FC = () => {
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+  const [selectedPrefs, setSelectedPrefs] = useState<number[]>([]);
+  const [graphData, setGraphData] = useState<Record<string, number>[] | null>(
+    null
+  );
 
+  // マウント時
   useEffect(() => {
     void (async () => {
       // RESAS-API から都道府県一覧のデータを取得
@@ -18,8 +27,32 @@ const App: React.FC = () => {
     })();
   }, []);
 
+  // 選択中の都道府県が変更されたとき
+  useEffect(() => {
+    void (async () => {
+      // 選択されている全ての都道府県について RESAS-API から人口構成（推移）のデータを取得
+      // レスポンス仕様: https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
+      const responses = await Promise.all(
+        selectedPrefs.map((pref) => fetchResasPopulation(pref))
+      );
+
+      // 選択中の都道府県名のリスト
+      const prefNames = selectedPrefs.map((prefCode) => {
+        const prefName = prefectures.find((pref) => pref.prefCode === prefCode)
+          ?.prefName;
+        return prefName ?? "";
+      });
+
+      // RESAS-API の人口構成のデータをグラフデータに変換
+      const graphData = resasPopulationToGraphData(prefNames, responses);
+
+      setGraphData(graphData);
+    })();
+  }, [selectedPrefs]);
+
+  // 都道府県のチェックボックス変更
   const handleChangePrefectures = (prefs: number[]) => {
-    console.log(prefs);
+    setSelectedPrefs(prefs);
   };
 
   return (
@@ -34,6 +67,45 @@ const App: React.FC = () => {
       ></PrefectureSelector>
     </div>
   );
+};
+
+/**
+ * RESAS-API の人口構成のデータをグラフデータに変換
+ * @param prefNames 都道府県名のリスト
+ * @param responses 都道府県ごとの RESAS-API のレスポンスのリスト
+ */
+const resasPopulationToGraphData = (
+  prefNames: string[],
+  responses: ResasPopulationResponse[]
+) => {
+  // 各都道府県の総人口のデータを抽出
+  const totalData: Record<string, number>[][] = responses
+    .map((response) => response.result.data)
+    .map(
+      (datum) =>
+        datum.filter(
+          (datum: ResasPopulationData) => datum.label === "総人口"
+        )[0].data
+    );
+
+  // 「年」のリスト
+  const years = [
+    ...new Set(totalData.flatMap((data) => data.map((record) => record.year))),
+  ];
+
+  // 総人口データの配列をグラフ用のデータに変換
+  const graphData = years.map((year: number) => {
+    const record: Record<string, number> = { 年: year };
+    totalData.forEach((data, index) => {
+      const value = data.find((datum) => datum.year === year)?.value;
+      if (value !== undefined) {
+        record[prefNames[index]] = value;
+      }
+    });
+    return record;
+  });
+
+  return graphData;
 };
 
 export default App;
